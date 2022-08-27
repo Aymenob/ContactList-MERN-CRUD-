b9e79b2499fc06f9d7fe4be252bd7cd08ce8a52e
Aymenob <aymenobba@gmail.com>
2022-08-27 05:05:25 +0200

    contactlist-MERN-CRUD

--- client/src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  counterSlice from "../Redux/slice"
-
-
-export const store = configureStore({
-  reducer: {
-   Users:counterSlice
-  }
-})
